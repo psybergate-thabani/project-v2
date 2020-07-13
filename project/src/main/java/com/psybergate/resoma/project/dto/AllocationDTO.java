cedeb11f987f9b40f7c94da3f7f2b46584d7f3f0
unknown <thabani@psybergate.co.za>
2020-07-13 16:10:41 +0200

    Added the startDate, expectedStartDate and endDate in the Allocation entity.
    Added the startDate, expectedStartDate and endDate in the Allocation when creating the entity table in liquibase changeset.

--- project/src/main/java/com/psybergate/resoma/project/dto/AllocationDTO.java
+++ project/src/main/java/com/psybergate/resoma/project/dto/AllocationDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -11,7 +13,18 @@ import java.util.UUID;
 @Data
 public class AllocationDTO {
 
+    @NotNull(message = "{projectid.notnull}")
     private UUID projectId;
 
+    @NotNull(message = "{employeeid.notblank}")
     private UUID employeeId;
+
+    @NotNull(message = "{startdate.notblank}")
+    private LocalDate startDate;
+
+    @NotNull(message = "{expectedenddate.notblank}")
+    private LocalDate expectedEndDate;
+
+    private LocalDate endDate;
+
 }
